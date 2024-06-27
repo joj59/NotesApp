bfccee50d0aa634b56516325490ad96b362c99a4
joj59 <jojijoseph@hotmail.co.uk>
2024-06-27 22:22:37 +0100

    cards color

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+let showModal = ref(false);
+const modelInput = ref('');
+const notes = ref([]);
+
+const hideModal = e => {
+    if (e.target.classList.contains('overlay')) {
+        console.log(notes);
+        showModal.value = false;
+    }
+};
+
+const createNote = () => {
+    notes.value.push({
+        id: Math.floor(Math.random() * 100000),
+        text: modelInput.value,
+        date: new Date(),
+        backgroundColor: 'hsl(' + Math.random() * 360 + ', 100%, 75%)',
+    });
+    showModal.value = false;
+    modelInput.value = '';
+};
+</script>
 
 <template>
     <main>
+        <div class="overlay" v-if="showModal" @click="hideModal">
+            <div class="modal">
+                <textarea v-model="modelInput" name="note" id="note" cols="30" rows="10"></textarea>
+                <button @click="createNote">Add Note</button>
+            </div>
+        </div>
         <div class="container">
             <header>
                 <h1>Notes</h1>
-                <button>+</button>
+                <button @click="showModal = true">+</button>
             </header>
             <div class="cards-container">
-                <div class="card">
-                    <p class="main-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, culpa!</p>
-                    <p class="date">27/06/2024</p>
+                <div class="card" v-for="note in notes" :key="note.id" :style="{ backgroundColor: note.backgroundColor }">
+                    <p class="main-text">{{ note.text }}</p>
+                    <p class="date">{{ note.date.toDateString() }}</p>
                 </div>
             </div>
         </div>
@@ -65,6 +95,32 @@ main {
                     font-size: 1rem;
                 }
             }
+        }
+    }
+
+    .overlay {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.384);
+        z-index: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .modal {
+            width: 60%;
+            background-color: white;
+            padding: 2rem;
+            border-radius: 3rem;
+            position: relative;
+            display: flex;
+            flex-direction: column;
+        }
+
+        button {
+            background-color: $newColor;
+            margin-top: 1rem;
         }
     }
 }
